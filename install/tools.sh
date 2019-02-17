60af71a07598bcf5b0c3fd5fa4adf69df2bcddd4
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2019-02-17 18:47:54 -0500

    add locales

--- install/tools.sh
+++ install/tools.sh
@@ -8,9 +8,10 @@ apt-get install -y vim wget net-tools locales bzip2 git \
     htop locales python-numpy #used for websockify/novnc
 apt-get clean -y
 
-echo "generate locales for C.UTF-8" #was en_US.UTF-8
-export LANGUAGE=en_US:en
-export LC_ALL=en_US.UTF-8
+echo "generate locales" #was en_US.UTF-8 or C.UTF-8
+export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
-locale-gen C.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
 dpkg-reconfigure locales
+localedef -i en_US -f UTF-8 en_US.UTF-8

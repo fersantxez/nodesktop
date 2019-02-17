3c2ba492c129000a2f240535dab9e8ad64751d57
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2019-02-17 18:45:28 -0500

    add locales

--- install/tools.sh
+++ install/tools.sh
@@ -9,8 +9,8 @@ apt-get install -y vim wget net-tools locales bzip2 git \
 apt-get clean -y
 
 echo "generate locales for C.UTF-8" #was en_US.UTF-8
-export LANGUAGE=C.UTF-8
-export LANG=C.UTF-8
-export LC_ALL=C.UTF-8
+export LANGUAGE=en_US:en
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
 locale-gen C.UTF-8
 dpkg-reconfigure locales

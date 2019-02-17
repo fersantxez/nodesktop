84dbaa00e802f7d7502eee89dfeefe1746bcf8a3
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2019-02-16 23:43:11 -0500

    change chromium for chrome

--- install/chrome.sh
+++ install/chrome.sh
@@ -2,10 +2,6 @@
 ### every exit != 0 fails the script
 set -e
 
-echo "Install Chromium Browser"
-apt-get update 
-apt-get install -y chromium chromium-l10n
-apt-get clean -y
-ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
-### fix to start chromium in a Docker container, see https://github.com/ConSol/docker-headless-vnc-container/issues/2
-echo "CHROMIUM_FLAGS='--no-sandbox --start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
+echo "Install Google Chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb

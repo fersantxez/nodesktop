b558822c5085e3e48a982aee12c362d4ce00eec2
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2019-02-17 20:23:57 -0500

    change to gdrive ocamlfuse

--- install/gdrive.sh
+++ install/gdrive.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Install Google Drive through Ocamlfuse"
-#https://www.ostechnix.com/how-to-mount-google-drive-locally-as-virtual-file-system-in-linux/
+#https://github.com/astrada/google-drive-ocamlfuse/wiki/Installation
 
 apt-get install -y software-properties-common dirmngr gnupg-agent
 add-apt-repository ppa:alessandro-strada/ppa
@@ -11,7 +11,7 @@ add-apt-repository ppa:alessandro-strada/ppa
 echo 'deb http://ppa.launchpad.net/alessandro-strada/ppa/ubuntu xenial main' | \
     sudo tee /etc/apt/sources.list.d/alessandro-strada-ubuntu-ppa.list >/dev/null
 #replace bionic at the end of both lines with xenial, for version > 0.6.21
-sed -i 's/bionic/xenial/g' /etc/apt/sources.list.d/alessandro-strada-ubuntu-ppa-bionic.list
+sed -i 's/bionic/xenial/g' /etc/apt/sources.list.d/alessandro-strada-ubuntu-ppa.list
 
 apt-get update
 apt-get install google-drive-ocamlfuse

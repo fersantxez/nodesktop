31ba966f407832ead2f6736e877296e52c6bf49b
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2019-02-17 20:19:33 -0500

    change to gdrive ocamlfuse

--- install/gdrive.sh
+++ install/gdrive.sh
@@ -8,7 +8,10 @@ echo "Install Google Drive through Ocamlfuse"
 apt-get install -y software-properties-common dirmngr gnupg-agent
 add-apt-repository ppa:alessandro-strada/ppa
 
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AD5F235DF639B041
-echo 'deb http://ppa.launchpad.net/alessandro-strada/ppa/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/alessandro-strada-ubuntu-ppa.list >/dev/null
+echo 'deb http://ppa.launchpad.net/alessandro-strada/ppa/ubuntu xenial main' | \
+    sudo tee /etc/apt/sources.list.d/alessandro-strada-ubuntu-ppa.list >/dev/null
+#replace bionic at the end of both lines with xenial, for version > 0.6.21
+sed -i 's/bionic/xenial/g' /etc/apt/sources.list.d/alessandro-strada-ubuntu-ppa-bionic.list
+
 apt-get update
 apt-get install google-drive-ocamlfuse

95b0ddb83a12dd98ae5395b4b6497e50c4b2e724
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2019-02-17 20:36:55 -0500

    change to gdrive ocamlfuse

--- install/gdrive.sh
+++ install/gdrive.sh
@@ -11,6 +11,7 @@ echo 'deb http://ppa.launchpad.net/alessandro-strada/ppa/ubuntu xenial main' | \
     sudo tee /etc/apt/sources.list.d/alessandro-strada-ubuntu-ppa-bionic.list >/dev/null
 #replace bionic at the end of both lines with xenial, for version > 0.6.21
 sed -i 's/bionic/xenial/g' /etc/apt/sources.list.d/alessandro-strada-ubuntu-ppa-bionic.list
+sed -i 's/disco/xenial/g' /etc/apt/sources.list.d/alessandro-strada-ubuntu-ppa-bionic.list
 sed -i 's/#deb-src/deb-src/g' /etc/apt/sources.list.d/alessandro-strada-ubuntu-ppa-bionic.list
 
 apt-get update

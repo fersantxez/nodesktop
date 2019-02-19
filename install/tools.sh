104b148a1d3a7d2d56f12754e5421220159db99e
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2019-02-18 20:40:37 -0500

    add user/group in install process

--- install/tools.sh
+++ install/tools.sh
@@ -14,3 +14,10 @@ sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
 export LANGUAGE=en_US:en
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
+
+### Add myself as a user if the variable was passed, otherwise nss_wrapper
+if [ -z "$NEWUSER" ]; then
+    echo -e "Adding new user and group: "${NEWUSER}
+    groupadd -g 5001 $NEWUSER \
+    && useradd -s /bin/bash -m -u 5001 -g $NEWUSER $NEWUSER
+fi

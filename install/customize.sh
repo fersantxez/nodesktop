5f6ec6f9373fb471fe3e7c5719b23c4e10f0be71
Your Name <you@example.com>
2019-02-20 04:18:59 +0000

    wait to decompress to copy

--- install/customize.sh
+++ install/customize.sh
@@ -3,8 +3,8 @@
 set -e
 
 ### configure customization
-tar xvfz $HOME/config_xfce4.tgz
-#delete archive after uncompressing
+tar xvfz $HOME/config_xfce4.tgz && \
+cp -R $HOME/.config /etc/skel && \
 rm -f $HOME/config_xfce4.tgz
 ### put under /etc/skel to turn into default config and avoid "first start"
-cp -R $HOME/.config /etc/skel
+

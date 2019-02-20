ac5b34dc3331ee625ff9a9253a3d375461206de2
Your Name <you@example.com>
2019-02-20 05:42:31 +0000

    remove default panel

--- install/customize.sh
+++ install/customize.sh
@@ -8,3 +8,7 @@ cp -R $HOME/.config /etc/skel && \
 rm -f $HOME/config_xfce4.tgz
 ### put under /etc/skel to turn into default config and avoid "first start"
 
+### renaming and moving /etc/xdg/xfce4/panel/default.xml to /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml will remove the prompt and use that file to create the default panel layout.
+mv -f /etc/xdg/xfce4/panel/default.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+rm -f $HOME/.config/xfce4/panel/default.xml 
+

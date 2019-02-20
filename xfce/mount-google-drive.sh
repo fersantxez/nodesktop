d8d5da4d277a32ff17cfae25e8f7234c779309df
Your Name <you@example.com>
2019-02-20 06:02:21 +0000

    add popup to google drive mount

--- xfce/mount-google-drive.sh
+++ xfce/mount-google-drive.sh
@@ -4,12 +4,14 @@ set -e
 
 #if this is the first time, launch ocamlfuse once to initialize -- this will launch browser for Oauth.
 if [ ! -f $HOME/.onlyonce ]; then
-    echo -e "ocamlfuse first launch - initialize without parameters"
-    /usr/bin/google-drive-ocamlfuse
-    exit
+    xterm -e "echo -e 'Google drive first launch - initializing'; \
+    /usr/bin/google-drive-ocamlfuse; \
+    read -p 'Please run again to mount the drive. Press any key to continue';  \
+    exit"
 fi
 #write state file to detect next launch
 touch $HOME/.onlyonce
 
 #otherwise launch again attaching home directory
-/usr/bin/google-drive-ocamlfuse ~/GoogleDrive
+xterm -e "read -p 'Google drive initialized. Press any key to mount your drive in ~/GoogleDrive'; \ 
+/usr/bin/google-drive-ocamlfuse ~/GoogleDrive"

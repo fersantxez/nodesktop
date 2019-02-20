3ce7ec152ceb4e202ba24cdd061d7a477192e0b9
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2019-02-19 22:23:59 -0500

    launch google drive in two stages

--- xfce/mount-google-drive.sh
+++ xfce/mount-google-drive.sh
@@ -2,9 +2,14 @@
 ### every exit != 0 fails the script
 set -e
 
-#launch ocamlfuse once to initialize -- this will launch browser for Oauth.
-#sleep 3
-#launch again attaching home directory
-/usr/bin/google-drive-ocamlfuse && \
-sleep 10 && \
-/usr/bin/google-drive-ocamlfuse $GDRIVE_MOUNT_DIR
+#if this is the first time, launch ocamlfuse once to initialize -- this will launch browser for Oauth.
+if [ ! -f $HOME/.onlyonce ]; then
+    echo -e "ocamlfuse first launch - initialize without parameters"
+    /usr/bin/google-drive-ocamlfuse
+    exit
+fi
+#write state file to detect next launch
+touch $HOME/.onlyonce
+
+#otherwise launch again attaching home directory
+/usr/bin/google-drive-ocamlfuse ~/GoogleDrive

3e9583de0691b41b39b348d7823327ba218ad208
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2019-02-26 16:46:59 -0500

    correct gcloud install

--- install/gcloud.sh
+++ install/gcloud.sh
@@ -7,6 +7,6 @@ echo "Install Google Cloud SDK"
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list 
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 
+curl --insecure https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 
 apt-get update -y 
 apt-get install google-cloud-sdk -y

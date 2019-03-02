78b4e9962bde1e680f288cad9986b152066c24ac
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2019-03-01 20:55:40 -0500

    cleanup in separate script

--- install/cleanup.sh
+++ install/cleanup.sh
@@ -3,4 +3,4 @@
 set -e
 
 sudo apt-get clean && \
-sudo apt-get remove --purge -y $BUILD_PACKAGES $(apt-mark showauto) && rm -rf /var/lib/apt/lists/*
+sudo apt-get remove --purge -y --allow-remove-essential $BUILD_PACKAGES $(apt-mark showauto) && rm -rf /var/lib/apt/lists/*

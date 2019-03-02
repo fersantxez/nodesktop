a515088f41cbbd0cbd90c9092df1830357e3a52b
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2019-03-01 21:12:21 -0500

    clean -y

--- install/cleanup.sh
+++ install/cleanup.sh
@@ -2,6 +2,6 @@
 ### every exit != 0 fails the script
 set -e
 
-sudo apt-get clean
+sudo apt-get clean -y
 #further clean - errors out
 #sudo apt-get remove --purge -y --allow-remove-essential $BUILD_PACKAGES $(apt-mark showauto) && rm -rf /var/lib/apt/lists/*

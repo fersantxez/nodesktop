a8ca7941ef52e59f84d5a69e3be3d4e4ddb67e4e
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2019-03-02 11:46:38 -0500

    fix statoverride on cleanup

--- install/cleanup.sh
+++ install/cleanup.sh
@@ -5,3 +5,9 @@ set -e
 sudo apt-get clean -y
 #further clean - errors out
 #sudo apt-get remove --purge -y --allow-remove-essential $BUILD_PACKAGES $(apt-mark showauto) && rm -rf /var/lib/apt/lists/*
+
+
+#fix statoverride for apt to work - remove crontab and Debian-exim groups
+cat /var/lib/dpkg/statoverride | tail -n +3 > ./statoverride && \
+sudo mv /var/lib/dpkg/statoverride /var/lib/dpkg/statoverride.bak && \
+sudo mv ./statoverride /var/lib/dpkg/statoverride

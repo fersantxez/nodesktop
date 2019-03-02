ca9b4b7939e05cfeb9829e4503be1528ec6e997f
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2019-03-02 12:00:11 -0500

    add xreader

--- install/xreader.sh
+++ install/xreader.sh
@@ -4,6 +4,9 @@ set -e
 
 echo "Install Xreader document viewer"
 
+### Install from packages
+
+## Debian packages
 sudo apt-get install -y \
 libcaja-extension1 \
 libnemo-extension1 \
@@ -25,11 +28,26 @@ libxslt1.1 \
 libwebkitgtk-3.0-0 \
 gstreamer1.0-plugins-good \
 libcaja-extension1 \
-libjs-mathjax
+libjs-mathjax \
+fonts-mathjax \
+fonts-mathjax-extras \
+fonts-stix \
+libjs-mathjax-doc
+
+
+## Mint backports 
 
 mkdir xreader
 cd xreader
 
+wget http://archive.ubuntu.com/ubuntu/pool/main/i/icu/libicu55_55.1-7_amd64.deb \
+http://archive.ubuntu.com/ubuntu/pool/main/g/geoclue/libgeoclue0_0.12.99-4ubuntu1_amd64.deb \
+http://archive.ubuntu.com/ubuntu/pool/main/libj/libjpeg8-empty/libjpeg8_8c-2ubuntu8_amd64.deb \
+http://archive.ubuntu.com/ubuntu/pool/main/libj/libjpeg-turbo/libjpeg-turbo8_1.4.2-0ubuntu3_amd64.deb \
+http://archive.ubuntu.com/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb \
+http://archive.ubuntu.com/ubuntu/pool/main/libw/libwebp/libwebp5_0.4.4-1_amd64.deb && \
+sudo dpkg -i ./*.deb && \
+sudo rm -f *.deb && \
 wget \
 http://packages.linuxmint.com/pool/backport/x/xreader/gir1.2-xreader_1.2.2%2bserena_amd64.deb \
 http://packages.linuxmint.com/pool/backport/x/xreader/libxreaderdocument3-dbg_1.2.2%2bserena_amd64.deb \
@@ -39,7 +57,32 @@ http://packages.linuxmint.com/pool/backport/x/xreader/libxreaderview3-dbg_1.2.2%
 http://packages.linuxmint.com/pool/backport/x/xreader/libxreaderview3_1.2.2%2bserena_amd64.deb \
 http://packages.linuxmint.com/pool/backport/x/xreader/xreader-common_1.2.2%2bserena_all.deb \
 http://packages.linuxmint.com/pool/backport/x/xreader/xreader-dbg_1.2.2%2bserena_amd64.deb \
-http://packages.linuxmint.com/pool/backport/x/xreader/xreader_1.2.2%2bserena_amd64.deb \
+http://packages.linuxmint.com/pool/backport/x/xreader/xreader_1.2.2%2bserena_amd64.deb && \
+sudo dpkg -i ./*.deb
 
 
+### Install from source
+#sudo apt install -y git dpkg-dev
+#
+#sudo apt install -y \
+#gobject-introspection libdjvulibre-dev libgail-3-dev          \
+#libgirepository1.0-dev libgtk-3-dev libgxps-dev               \
+#libkpathsea-dev libpoppler-glib-dev libsecret-1-dev           \
+#libspectre-dev libtiff-dev libwebkit2gtk-4.0-dev libxapp-dev  \
+#mate-common meson xsltproc yelp-tools
+
+#git clone https://github.com/linuxmint/xreader.git && \
+#cd xreader && \
+#meson debian/build \
+#        --prefix=/usr/local \
+#        --buildtype=plain \
+#        -D deprecated_warnings=false \
+#        -D djvu=true \
+#        -D dvi=true \
+#        -D t1lib=true \
+#        -D pixbuf=true \
+#        -D comics=true \
+#        -D introspection=true && \
+#ninja -C debian/build && \
+#ninja -C debian/build install
 

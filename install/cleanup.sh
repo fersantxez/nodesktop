911f0084ba13b9294a2d6981a8946347a4d6d2e2
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2019-03-01 21:06:04 -0500

    remove ultra-clean essentials

--- install/cleanup.sh
+++ install/cleanup.sh
@@ -2,5 +2,6 @@
 ### every exit != 0 fails the script
 set -e
 
-sudo apt-get clean && \
-sudo apt-get remove --purge -y --allow-remove-essential $BUILD_PACKAGES $(apt-mark showauto) && rm -rf /var/lib/apt/lists/*
+sudo apt-get clean
+#further clean - errors out
+#sudo apt-get remove --purge -y --allow-remove-essential $BUILD_PACKAGES $(apt-mark showauto) && rm -rf /var/lib/apt/lists/*

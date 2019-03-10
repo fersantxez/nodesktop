434d31f1c9075a34d73fb4a4735ba5daa1255c5a
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2019-03-10 18:59:34 -0400

    default to Adaptive theme for Sublime

--- install/sublime.sh
+++ install/sublime.sh
@@ -18,6 +18,7 @@ mkdir -p /headless/.config/sublime-text-3/Packages/User/Dank_Neon && \
 cd /headless/.config/sublime-text-3/Packages/User/Dank_Neon && \
 wget https://raw.githubusercontent.com/DankNeon/sublime/master/Dank_Neon.tmTheme && \
 echo '{
-        "color_scheme": "Packages/User/Dank_Neon/Dank_Neon.tmTheme"
+        "color_scheme": "Packages/User/Dank_Neon/Dank_Neon.tmTheme",
+        "theme": "Adaptive.sublime-theme"
 }
 ' > /headless/.config/sublime-text-3/Packages/User/Preferences.sublime-settings

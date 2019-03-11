686e35e50bd9d8c3ea84b810b4ffb574a3d77e35
fer <fernandosanchezmunoz@google.com>
2019-03-11 20:55:26 +0000

    add xscreensaver

--- install/tools.sh
+++ install/tools.sh
@@ -5,7 +5,7 @@ set -e
 echo "Install some common tools for further installation"
 apt-get update 
 apt-get install -y vim wget curl net-tools locales bzip2 git sudo gnupg-agent openssh-client openssh-server \
-    iputils-* transmission htop locales software-properties-common dirmngr python-numpy #used for websockify/novnc
+    iputils-* transmission htop locales software-properties-common dirmngr python-numpy xscreensaver
 apt-get clean -y
 
 ### Hide userland threads for HTop

0f7b21ba1b0279bb16df61f81af6b49fb30b5937
Your Name <you@example.com>
2019-03-10 21:45:49 -0400

    add tor from source

--- install/tor.sh
+++ install/tor.sh
@@ -4,21 +4,23 @@ set -e
 
 echo "Install Tor"
 
-sudo apt-get install -y tor
+sudo apt-get install -y apt-transport-https
 
 #Tor stretch repo
 tee /etc/apt/sources.list.d/tor-stretch.list <<-EOF
 deb     http://deb.torproject.org/torproject.org stretch main
 EOF
+gpg --keyserver keys.gnupg.net --recv-keys 886DDD89
 
 #Tor Browser backport
 tee /etc/apt/sources.list.d/stretch-backports.list <<-EOF
-deb http://deb.debian.org/debian stretch-backports main contrib
+deb https://deb.torproject.org/torproject.org stretch main
+deb-src https://deb.torproject.org/torproject.org stretch main
 EOF
 
-gpg --keyserver keys.gnupg.net --recv-keys 886DDD89
+curl https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+
 apt-get update -y 
-apt-get install -y deb.torproject.org-keyring
-apt-get install -y tor  
+apt-get install -y tor deb.torproject.org-keyring
 apt-get install -y torbrowser-launcher -t stretch-backports

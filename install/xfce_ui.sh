5f1c221627f82f27ffe7f5ce383b8b16440e7393
Your Name <you@example.com>
2019-03-17 20:53:46 -0400

    add gigolo

--- install/xfce_ui.sh
+++ install/xfce_ui.sh
@@ -5,5 +5,5 @@ set -e
 echo "Install Xfce4 UI components"
 apt-get update 
 apt-get install -y supervisor xfce4 xfce4-terminal xterm xfce4-goodies
-apt-get purge -y pm-utils xscreensaver*
+apt-get purge -y pm-utils
 apt-get clean -y

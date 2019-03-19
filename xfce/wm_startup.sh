3816a455ab3ce09296d80f9a6bbbd4f4ed8e6b24
Your Name <you@example.com>
2019-03-19 12:55:14 -0400

    change chown for /Users/fersanchez to wm_startup

--- xfce/wm_startup.sh
+++ xfce/wm_startup.sh
@@ -19,3 +19,8 @@ cat $HOME/wm.log
 ### hack to kill redundant panel
 export FIRST_PANEL_PID=$(ps aux|grep xfce4-panel| head -n1| awk {'print $2'})
 kill $FIRST_PANEL_PID
+
+#link /home/$USER with /headless
+USER=$(whoami)
+ln -s /headless /home/${USER}
+sudo chown ${USER}:${USER} /home/${USER}

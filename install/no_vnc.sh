9d129ea80e23516417c347597cf682cf07d3fc66
Your Name <you@example.com>
2019-06-18 21:44:54 -0400

    remove copy cert on install

--- install/no_vnc.sh
+++ install/no_vnc.sh
@@ -12,6 +12,3 @@ wget -qO- https://github.com/novnc/websockify/archive/v0.6.1.tar.gz | tar xz --s
 chmod +x -v $NO_VNC_HOME/utils/*.sh
 ## create index.html to forward automatically to `vnc_lite.html`
 ln -s $NO_VNC_HOME/vnc_lite.html $NO_VNC_HOME/index.html
-
-#copy cert from /
-cp -r self.pem $NO_VNC_HOME/self.pem

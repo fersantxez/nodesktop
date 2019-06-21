9beee220424cb720f4c18436fbc50801d8309e4b
Your Name <you@example.com>
2019-06-20 20:14:41 -0400

    sleep forever debug

--- scripts/vnc_startup.sh
+++ scripts/vnc_startup.sh
@@ -103,7 +103,7 @@ sudo openssl req \
 ## start vncserver and noVNC webclient
 echo -e "\n------------------ start noVNC  ----------------------------"
 if [[ $DEBUG == true ]]; then echo "$NO_VNC_HOME/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT"; fi
-$NO_VNC_HOME/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT --cert $NO_VNC_HOME/self.pem &> $STARTUPDIR/no_vnc_startup.log &
+$NO_VNC_HOME/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &> $STARTUPDIR/no_vnc_startup.log &
 PID_SUB=$!
 
 echo -e "\n------------------ start VNC server ------------------------"

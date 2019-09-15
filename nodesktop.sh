b696b7dbb0ac14ede09cf0689d7e5982fb6eaecd
Your Name <you@example.com>
2019-09-15 13:00:56 -0400

    clean up and change name of launch script

--- nodesktop.sh
+++ nodesktop.sh
@@ -51,25 +51,53 @@ ${NC}"
 # Functions
 # =============================================================================
 
-display_usage() { 
-    echo "usage: nodesktop -n name -w password [-p novnc_port] [-m machine_type] [-i image_string] [-s boot_disk_size] [-k docker_image] [-d color_depth] [-r resolution] [-H home_mount_dir][-R root_mount_dir] "
-    echo "  -n, --name 				NAME 			-name of this nodesktop instance"
-    echo "  -w, --password 			mypassword 		-Password to log into this server"
-    echo "  -p, --port 				6901 			-TCP port to listen on for noVNC connections"
-    echo "  -m, --machine_type 		n1-standard-2 	-GCE machine type"
-    echo "  -i, --image_string 		debian-9 		-String to find VM image to use in catalog"
-    echo "  -s, --boot_disk_size 	100GB 			-Boot disk size"
-    echo "  -k, --docker_image 		fernandosanchez/nodesktop:latest"
-    echo "  -d, --color_depth 		24				-Color depth for noVNC desktop"
-    echo "  -r, --resolution 		1280x1024		-Resolutionfor noVNC desktop"
-    echo "  -H, --home_mount_dir 	/mnt/home		-Mount point for the home directory in host"
-    echo "  -R, --root_mount_dir 	/mnt/root		-Mount point for the root directory in host"
-    exit 1
-	} 
+print_usage() { 
+    cat <<EOM
+
+    Usage:
+
+    $(basename $0) -n name -w password [-p novnc_port] [-m machine_type] [-i image_string] [-s boot_disk_size] [-k docker_image] [-d color_depth] [-r resolution] [-H home_mount_dir][-R root_mount_dir] "
+
+        [Parameter]					[Example]				[Description]
+		-n,--name 					NAME 				-name of this nodesktop instance"
+		-w,--password 				mypassword 			-Password to log into this server"
+		-p,--port 					6901 				-TCP port to listen on for noVNC connections"
+		-m,--machine_type 			n1-standard-2 		-GCE machine type"
+		-i,--image_string 			debian-9 			-String to find VM image to use in catalog"
+		-s, --boot_disk_size 		100GB 			-Boot disk size"
+		-k, --docker_image 			fernandosanchez/nodesktop:latest"
+		-d, --color_depth 			24				-Color depth for noVNC desktop"
+		-r, --resolution 			1280x1024		-Resolutionfor noVNC desktop"
+		-H, --home_mount_dir 		/mnt/home		-Mount point for the home directory in host"
+		-R, --root_mount_dir 		/mnt/root		-Mount point for the root directory in host"
+EOM
+exit 1
+}
 
 # Prefixes output and writes to STDERR:
 error() {
 	echo -e "\n\n${RED}nodesktop error${NC}: $@\n" >&2
+	print_usage
+	exit 1 #Exit on error
+}
+
+# Display a formatted message to shell:
+message() {
+	echo -e "\n** $@" >&2
+}
+
+# Display a formatted WARNING message to shell:
+warning() {
+	echo -e "\n\n${RED}nodesktop warning${NC}: $@\n" >&2
+}
+
+# Ask for user input and return the validated response:
+input() {
+	#redirect echo to >&2 so that it doesnt return this
+	echo -e "\n${BLUE}** ${@}${NC}" >&2
+	read -p "" user_input
+	#return string through echo
+	echo $user_input
 }
 
 # Checks for command presence in $PATH, errors:
@@ -120,11 +148,11 @@ enable_firewall_for_tag() {
 	TESTTAG=$1
 	TESTPORT=$2
 
-	printf '%-50s' " - $TESTTAG..."
+	printf '%-50s' " - $TESTPORT..."
 	
 	if [[ ! $(gcloud compute firewall-rules list --format=json|grep $TESTTAG) ]];then
-		echo -e "[CLOSED]"
-		printf "Opening firewall port for "$TESTTAG" ..."
+		# We have no rule for this tag:
+		echo -e "${RED}[ CLOSED ]${NC}"
 
 		gcloud compute firewall-rules create  \
 			$TESTTAG \
@@ -138,12 +166,106 @@ enable_firewall_for_tag() {
 			> /dev/null 2>&1
 		if [ ! $? -eq 0 ]; then
 			error "Error opening port "$TESTPORT" for tag "$TESTTAG". Please check your privileges."
-			exit 1
 		fi
 	else
-		echo -e "[OPEN]"
+		#We have a rule for this tag but it may be for a different port. Always update rule with TESTPORT
+		#Get all ports included in the rule
+		PORTS=$(gcloud compute firewall-rules describe $TESTTAG --format=flattened|grep ports|awk '{print $2}')
+		#Add all existing ports with the required tcp: prefix
+		for i in ${PORTS}; do
+			export PORTS_LIST="tcp:"${i}","${PORTS_LIST}
+		done
+		#Add the new port
+		export PORTS_LIST=${PORTS_LIST}"tcp:"${TESTPORT}
+		#Update the rule with the new list of ports including existing and new
+		gcloud compute firewall-rules update \
+			$TESTTAG \
+			--priority=1000 \
+			--rules=${PORTS_LIST} \
+			--source-ranges=0.0.0.0/0 \
+			--target-tags=$TESTTAG \
+			> /dev/null 2>&1
+		echo -e "${BLUE}[ OPEN ]${NC}"
 	fi
 }
+
+# =============================================================================
+# Base sanity checking
+# =============================================================================
+
+# Check for our required binaries:
+echo -e "** Checking for required binaries..."
+check_command gcloud "** Please install the Google Cloud SDK from: https://cloud.google.com/sdk/downloads"
+
+# This executes all the gcloud commands in parallel and then assigns them to separate variables:
+# Needed for non-array capabale bashes, and for speed.
+echo -e "** Checking gcloud variables..."
+PARAMS=$(cat <(gcloud_activeconfig_intercept config get-value compute/zone) \
+	<(gcloud_activeconfig_intercept config get-value compute/region) \
+	<(gcloud_activeconfig_intercept config get-value project) \
+	<(gcloud_activeconfig_intercept auth application-default print-access-token))
+read GCP_ZONE GCP_REGION GCP_PROJECT GCP_AUTHTOKEN <<<$(echo $PARAMS)
+
+# Check for our requisiste gcloud parameters:
+check_config $GCP_PROJECT "project"
+check_config $GCP_REGION "compute/region"
+check_config $GCP_ZONE "compute/zone"
+
+# Check credentials are set:
+printf '%-50s' " - 'application-default access token'..."
+if [[ $GCP_AUTHTOKEN == *"ERROR"* ]]; then
+	echo "[ UNSET ]"
+	error "You do not have application-default credentials set, please run this command:
+	gcloud auth application-default login"
+	exit 1
+fi
+echo "[ OK ]"
+
+# =============================================================================
+# Initialization and idempotent test/setting
+# =============================================================================
+
+# List of required APIs:
+REQUIRED_APIS="
+	compute
+	dns
+	storage-api
+	storage-component
+"
+
+# Bulk parallel process all of the API enablement:
+echo -e "** Checking requisiste GCP APIs..."
+
+# Read-in our currently enabled APIs, less the googleapis.com part:
+GCP_CURRENT_APIS=$(gcloud services list | grep -v NAME | cut -f1 -d'.')
+
+# Keep track of whether we modified the API state for friendliness:
+ENABLED_ANY=1
+
+for REQUIRED_API in $REQUIRED_APIS; do
+	if [ $(grep -q $REQUIRED_API <(echo $GCP_CURRENT_APIS))$? -eq 0 ]; then
+		# It's already enabled:
+		printf '%-50s' " - $REQUIRED_API"
+		echo "[ ON ]"
+	else
+		# It needs to be enabled:
+		printf '%-50s' " + $REQUIRED_API"
+		enable_api $REQUIRED_API.googleapis.com &
+		ENABLED_ANY=0
+		echo "[ OFF ]"
+	fi
+done
+
+# If we've enabeld any API, wait for child processes to finish:
+if [ $ENABLED_ANY -eq 0 ]; then
+	printf '%-50s' "**  Concurrently enabling APIs..."
+	wait
+
+else
+	printf '%-50s' "** API status..."
+fi
+echo "[ OK ]"
+
 
 # =============================================================================
 # Argument parsing
@@ -198,20 +320,24 @@ case $i in
     HOME_MOUNT_DIR="${i#*=}"
     shift # past argument=value
     ;;
+    -h=*|--help=*)
+	print_usage
+	shift
+    ;;
     *)
     # unknown option
-    display_usage
+    warning "UNKNOWN OPTION: "$i
+    print_usage
     ;;
 esac
 done
 
-
 # =============================================================================
-# Argument check and correction
+# Argument validation and correction
 # =============================================================================
 
 #Ensure all parameters are defined or ask for them interactively
-# List of requisite parameters:
+# List of required parameters:
 REQUIRED_PARAMS="
 	NAME
 	PASSWORD
@@ -231,11 +357,11 @@ for PARAM in $REQUIRED_PARAMS; do
 	fi
 done
 
-#NAME
-while [ $(echo ${NAME} | awk '{print length}') -ge 10 ]; do 
-	echo -e "** ERROR: NAME should be 10 characters long or less"
-	read -p "** Enter a new value for NAME: " NAME
-done
+#NAME - FIXME: no need for this to be 10chars or less anymore?
+#while [ $(echo ${NAME} | awk '{print length}') -ge 10 ]; do 
+#	echo -e "** ERROR: NAME should be 10 characters long or less"
+#	read -p "** Enter a new value for NAME: " NAME
+#done
 
 #HOME_MOUNT_DIR
 while [ ${HOME_MOUNT_DIR:0:1} != "/" ]; do 
@@ -277,83 +403,6 @@ fi
 
 export IMAGE=$(gcloud compute images list|grep ${IMAGE_STRING}|head -n 1|awk -s {'print $1'})
 
-
-# =============================================================================
-# Base sanity checking
-# =============================================================================
-
-# Check for our requisite binaries:
-echo -e "** Checking for requisite binaries..."
-check_command gcloud "** Please install the Google Cloud SDK from: https://cloud.google.com/sdk/downloads"
-
-# This executes all the gcloud commands in parallel and then assigns them to separate variables:
-# Needed for non-array capabale bashes, and for speed.
-echo -e "** Checking gcloud variables..."
-PARAMS=$(cat <(gcloud_activeconfig_intercept config get-value compute/zone) \
-	<(gcloud_activeconfig_intercept config get-value compute/region) \
-	<(gcloud_activeconfig_intercept config get-value project) \
-	<(gcloud_activeconfig_intercept auth application-default print-access-token))
-read GCP_ZONE GCP_REGION GCP_PROJECT GCP_AUTHTOKEN <<<$(echo $PARAMS)
-
-# Check for our requisiste gcloud parameters:
-check_config $GCP_PROJECT "project"
-check_config $GCP_REGION "compute/region"
-check_config $GCP_ZONE "compute/zone"
-
-# Check credentials are set:
-printf '%-50s' " - 'application-default access token'..."
-if [[ $GCP_AUTHTOKEN == *"ERROR"* ]]; then
-	echo "[ UNSET ]"
-	error "You do not have application-default credentials set, please run this command:
-	gcloud auth application-default login"
-	exit 1
-fi
-echo "[ OK ]"
-
-# =============================================================================
-# Initialization and idempotent test/setting
-# =============================================================================
-
-# List of requisite APIs:
-REQUIRED_APIS="
-	compute
-	dns
-	storage-api
-	storage-component
-"
-
-# Bulk parallel process all of the API enablement:
-echo -e "** Checking requisiste GCP APIs..."
-
-# Read-in our currently enabled APIs, less the googleapis.com part:
-GCP_CURRENT_APIS=$(gcloud services list | grep -v NAME | cut -f1 -d'.')
-
-# Keep track of whether we modified the API state for friendliness:
-ENABLED_ANY=1
-
-for REQUIRED_API in $REQUIRED_APIS; do
-	if [ $(grep -q $REQUIRED_API <(echo $GCP_CURRENT_APIS))$? -eq 0 ]; then
-		# It's already enabled:
-		printf '%-50s' " - $REQUIRED_API"
-		echo "[ ON ]"
-	else
-		# It needs to be enabled:
-		printf '%-50s' " + $REQUIRED_API"
-		enable_api $REQUIRED_API.googleapis.com &
-		ENABLED_ANY=0
-		echo "[ OFF ]"
-	fi
-done
-
-# If we've enabeld any API, wait for child processes to finish:
-if [ $ENABLED_ANY -eq 0 ]; then
-	printf '%-50s' "**  Concurrently enabling APIs..."
-	wait
-
-else
-	printf '%-50s' "** API status..."
-fi
-echo "[ OK ]"
 
 # =============================================================================
 # Open firewall ports

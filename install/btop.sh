401bcd2ae3bca6b343fd6ffe26eface31282dbe3
Fernando Sanchez <fersanchez@google.com>
2023-02-03 12:10:45 -0500

    fix btop launcher/installer

--- install/btop.sh
+++ install/btop.sh
@@ -6,14 +6,14 @@ echo "Install btop (Bashtop C++)"
 
 wget https://github.com/aristocratos/btop/releases/download/v1.2.13/btop-x86_64-linux-musl.tbz
 tar -xjf btop-x86_64-linux-musl.tbz
-mv btop/bin/btop /usr/btop
+mv btop/bin/btop /usr/bin/btop
 rm -Rf btop*
 
 cat <<EOF >> /headless/Desktop/btop.desktop 
 [Desktop Entry]
 Version=1.0
 Type=Application
-Name=btop
+Name=/usr/bin/btop
 Comment=Display information about the running system
 Exec=btop
 Icon=utilities-system-monitor

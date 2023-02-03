4a6a5ac6704a5a73461c284022b4d5e1eec9db04
Fernando Sanchez <fersanchez@google.com>
2023-02-03 14:46:29 -0500

    correct tor icon

--- install/tor.sh
+++ install/tor.sh
@@ -18,5 +18,19 @@ sudo mv tor-browser/* /opt/tor && \
 sudo rm -Rf tor-browser && \
 sudo chmod a+x -R /opt/tor/ && \
 sudo chown 1000:1000 -R /opt/tor && \
-sudo mv /opt/tor/start-tor-browser.desktop /headless/Desktop/tor.desktop && \
 sudo chown 1000:1000 /headless/Desktop/tor
+
+#overwrite desktop launcher
+
+cat <<EOF >> /headless/Desktop/tor.desktop 
+[Desktop Entry] 
+Type=Application
+Name=Tor Browser Setup
+GenericName=Web Browser
+Comment=Tor Browser is +1 for privacy and −1 for mass surveillance
+Categories=Network;WebBrowser;Security;
+Exec=sh -c /opt/tor/Browser/start-tor-browser --detach
+X-TorBrowser-ExecShell=./Browser/start-tor-browser --detach
+Icon=tor-browser
+StartupWMClass=Tor Browser
+EOF

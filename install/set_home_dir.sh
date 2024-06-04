db82c05924ac35258062cd2a7f20bf77dffc2cd2
fer <fer@linuxdev.lan>
2024-06-04 14:37:23 -0400

    make desktop launchers trusted

--- install/set_home_dir.sh
+++ install/set_home_dir.sh
@@ -19,5 +19,6 @@ export SHELL=/usr/bin/bash
 #mark all desktop shortcuts as executable
 for file in /headless/Desktop/*.desktop; do
   sudo chmod a+x $file
+  sudo chown ${USER}:${USER} $file
   sudo /usr/bin/gio set $file "metadata::trusted" yes
 done

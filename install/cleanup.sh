eaccea527c25d4d2489a8d45e816946b3422fcc1
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2024-06-05 17:22:09 -0300

    slim version

--- install/cleanup.sh
+++ install/cleanup.sh
@@ -12,7 +12,7 @@ sudo mv /var/lib/dpkg/statoverride /var/lib/dpkg/statoverride.bak && \
 sudo mv ./statoverride /var/lib/dpkg/statoverride
 
 #clean up home directory
-sudo rm /headless/*rar*
+#sudo rm /headless/*rar*
 #sudo rm /headless/wm_startup.sh #this breaks startup
 sudo rm -Rf /headless/install
 sudo rm -f /headless/core #FIXME -- where is this core coming from?

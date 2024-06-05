ce2567b3fa3ff72d4700f69d2c27ee62e0226b54
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2024-06-05 10:52:07 -0300

    ensure Desktop exists before installers

--- install/google-drive-ocamlfuse.sh
+++ install/google-drive-ocamlfuse.sh
@@ -39,5 +39,5 @@ Terminal=false
 StartupNotify=false
 EOF
 #Executable and trusted
-chmod 755 /headless/Desktop/gdrive.desktop
+chmod a+x /headless/Desktop/gdrive.desktop
 gio set /headless/Desktop/gdrive.desktop "metadata::trusted" yes
